14ee8af939e080498b5938e29f3795b97178f736
amirali rezaei <exjnnd@gmail.com>
2021-04-25 10:41:43 +0430

    My init

---
+++ js/Main.js
@@ -0,0 +1,23 @@
+function P1() {
+    const elm = document.getElementById("PText");
+    elm.innerHTML = "JavaScript, React, Vue";
+}
+
+function P2() {
+    const elm = document.getElementById("PText2");
+    elm.innerHTML = "برنامه نویسی وب با Asp.Net Core";
+}
+
+function P3() {
+    const elm = document.getElementById("PText3");
+    elm.innerHTML = "طراحی سایت با html, css, react";
+}
+
+function Clear() {
+    const elm = document.getElementById("PText");
+    const elm2 = document.getElementById("PText2");
+    const elm3 = document.getElementById("PText3");
+    elm.innerHTML = "";
+    elm2.innerHTML = "";
+    elm3.innerHTML = "";
+}
